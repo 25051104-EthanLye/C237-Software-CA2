2c36cd9facbf26668491336462dd5b5971a624d3
Ethan Lye <25051104@myrp.edu.sg>
2026-07-14 16:24:26 +0800

    Setting up complete

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+
+const flash = require('connect-flash');
+
+const app = express();
+
+// Database connection
+const db = mysql.createConnection({
+    host: 'c237-yewyih-mysql.mysql.database.azure.com',
+    user: 'c237_006',
+    password: 'c237006@2026!',
+    database: 'c237_006_ca2_db1',
+    ssl: {}
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// PATHS GO DOWN HERE //
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// END OF PATHS // 
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
